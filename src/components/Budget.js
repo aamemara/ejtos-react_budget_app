a3356507fd0ada463db9f85a04fdec4ab26abc15
aamemara <121405693+aamemara@users.noreply.github.com>
2023-01-02 21:22:31 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,40 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { totalExpenses,currency,budget,dispatch  } = useContext(AppContext);
+
+    const setBudget = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        });
+    }
+    const editBudget = (value) => {
+        if(!isNaN(value)){
+            if (value > 20000) {
+                alert("The value cannot exceed 20,000");
+                return;
+            }
+            if(value < parseInt(totalExpenses)) {
+                alert("You cannot reduce the budget value lower than the spending");
+                return;
+            }
+            setBudget(value);
+        }
+    }
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}
+                <input
+                required='required'
+                type='number'
+                id='cost'
+                value={budget}
+                style={{ size: 10}}
+                onChange={(event) => editBudget(event.target.value)}>
+                </input>
+            </span>
         </div>
     );
 };
